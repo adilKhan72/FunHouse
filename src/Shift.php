d115694f37cd4abd4be9254e537a2970b493565d
Adil Khan <adil.khan@theshopworks.com>
2023-05-11 17:32:19 +0500

    logics and coding

--- src/Shift.php
+++ src/Shift.php
@@ -11,5 +11,33 @@ class Shift
     protected $startTime;
     protected $endTime;
     protected $breaks;
+    
+    public function __construct($employee, DateTime $startTime, DateTime $endTime, array $breaks = [])
+    {
+        $this->employee = $employee;
+        $this->startTime = $startTime;
+        $this->endTime = $endTime;
+        $this->breaks = $breaks;
+    }
+
+    public function getEmployee()
+    {
+        return $this->employee;
+    }
+
+    public function getStartTime()
+    {
+        return $this->startTime;
+    }
+
+    public function getEndTime()
+    {
+        return $this->endTime;
+    }
+    
+    public function getBreaks()
+    {
+        return $this->breaks;
+    }
 
 }
