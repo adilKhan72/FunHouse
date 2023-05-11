e5885069559f08fdeb1fd987f13893a1e177770a
Adil Khan <adil.khan@theshopworks.com>
2023-05-11 17:24:15 +0500

    php unittest lib and added gitignore

--- tests/SingleManningCalculatorTest.php
+++ tests/SingleManningCalculatorTest.php
@@ -1,13 +1,20 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/SingleManningCalculator.php';
 require_once __DIR__ . '/../src/Rota.php';
 require_once __DIR__ . '/../src/Shift.php';
 
+use PHPUnit\Framework\TestCase;
 
 class SingleManningCalculatorTest extends TestCase
 {
     protected $calculator;
 
-  
+    protected function setUp(): void
+    {
+        $this->calculator = new SingleManningCalculator();
+    }
+
+    // Add more test methods for other scenarios or additional tests
 }
